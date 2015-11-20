dea70eba3986de7b1d439c2f8a041ac199b1d969
Claude Falguiere <cfalguiere@gmail.com>
2015-11-20 23:07:22 +0100

    Ajout du filtre de rendering de carte https://github.com/cfalguiere/LabRPG/issues/11

---
+++ WebApp/test/spec/filters/rendercard.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('Filter: renderCard', function () {
+
+  // load the filter's module
+  beforeEach(module('labrpgApp'));
+
+  // initialize a new instance of the filter before each test
+  var render;
+  beforeEach(inject(function ($filter) {
+    render = $filter('render');
+  }));
+
+  describe('render', function() {
+
+    it('has a render Filter', function() {
+          expect(render).toBeDefined();
+    });
+
+    it('should return a list of CSS classes', function() {
+          var cell1 = { id: 1, card: { id: 1, color: 'red', shape: 'heart'}, state: 'placed' };
+          expect(render(cell1)).toBe('glyphicon glyphicon-heart card card-placed card-red');
+    });
+
+  });
+
+
+});
